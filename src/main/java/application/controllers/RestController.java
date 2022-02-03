acd9002bf2a4e3c4aad539d14fbcec8ebe773be9
Nicknameee <98988322+Nicknameee@users.noreply.github.com>
2022-02-04 01:12:06 +0200

    Add files via upload

---
+++ src/main/java/application/controllers/RestController.java
@@ -0,0 +1,47 @@
+package application.controllers;
+
+import application.models.UserModel;
+import application.service.DataInteractionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("/application")
+public class RestController
+{
+    private DataInteractionService service;
+
+    @Autowired
+    public void setService(DataInteractionService service)
+    {
+        this.service = service;
+    }
+
+    @GetMapping("/readGET")
+    public UserModel read(@ModelAttribute("object") UserModel userModel)
+    {
+        return service.getRepositoryUser().getByLogin(userModel.getLogin());
+    }
+    @PostMapping("/createPOST")
+    public UserModel create(@ModelAttribute("object") UserModel userModel)
+    {
+        service.getRepositoryUser().save(userModel);
+        return userModel;
+    }
+    @PutMapping("/updatePUT")
+    public UserModel update(@ModelAttribute("object") UserModel userModel)
+    {
+        service.getRepositoryUser().update(userModel.getLogin() , userModel);
+        return userModel;
+    }
+    @DeleteMapping("/deleteDELETE")
+    public String delete(@ModelAttribute("object") UserModel userModel)
+    {
+        if (service.getRepositoryUser().getByLogin(userModel.getLogin()) != null)
+        {
+            service.getRepositoryUser().delete(userModel.getLogin());
+            return "DELETED SUCCESSFULLY";
+        }
+        return "NO COINCIDENCE WERE FOUND";
+    }
+}
